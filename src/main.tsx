f32444b86ed215f2f4ef8de995cd6628ccba8b44
beste-apiumhub <beste.burcu@apiumhub.com>
2023-03-28 18:25:59 +0200

    Add project routes for podcast and episode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App'
+import { Episode } from './components/episode';
+import { Podcast } from './components/podcast';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "podcast/:podcastId",
+    element: <Podcast />,
+    children:[{
+      path:  "episode/:episodeId",
+      element: <Episode />,
+    }]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
