e8806706323a7b7a74b93a8ee6ba59acb3153261
beste-apiumhub <beste.burcu@apiumhub.com>
2023-03-29 12:20:09 +0200

    Add new styles for title, autor and podcastDetail

--- src/components/listOfPodcasts.tsx
+++ src/components/listOfPodcasts.tsx
@@ -15,8 +15,8 @@ export const ListOfPodcasts = ({ podcasts }: { podcasts: Podcast[] }): JSX.Eleme
         podcasts.map((podcast: Podcast) => (
           <article className="card" onClick={handleClick}>
             <header><img src={podcast.imageSource} id={podcast.id} alt="Hot air balloons" style={{ borderRadius: '50%' }} /></header>
-            <main><h3>{podcast.title}</h3>
-              <h4>Autor: {podcast.autor}</h4></main>
+            <main><h3 className="title">{podcast.title}</h3>
+              <h4 className="autor">Autor: {podcast.autor}</h4></main>
           </article>))
       }
     </div>)
