182500a3a3b87811fb4740b9febf3309aa23fa13
beste-apiumhub <beste.burcu@apiumhub.com>
2023-03-30 00:19:55 +0200

    Add localStorage to check if the api call is from one day before

--- src/hooks/usePodcasts.ts
+++ src/hooks/usePodcasts.ts
@@ -1,28 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getPodcastsFromService } from "../services/podcasts";
 
 export function usePodcasts({ filter }: { filter: string }) {
   const [mappedPodcasts, setMappedPodcasts] = useState([])
 
-  const getPodcasts = async () => {
-    await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw new Error('Network response was not ok.')
-      })
-      .then(data => {
-        const podcasts = JSON.parse(data.contents).feed.entry
-        const mappedPodcasts = podcasts.map((podcast: any) => ({
-          id: podcast.id.attributes["im:id"],
-          title: podcast["im:name"].label,
-          autor: podcast["im:artist"].label,
-          imageSource: podcast["im:image"][2].label,
-          description: podcast.summary.label
-        }));
-        setMappedPodcasts(mappedPodcasts);
-      });
-  }
+  useEffect(() => {
+    const getPodcasts = async () => {
+      const filteredPodcasts = await getPodcastsFromService({ filter })
+      setMappedPodcasts(filteredPodcasts)
+    }
+    getPodcasts();
+  }, [filter])
 
-  return { mappedPodcasts, getPodcasts }
+  return { mappedPodcasts }
 }
--- src/services/podcasts.ts
+++ src/services/podcasts.ts
@@ -1,19 +1,35 @@
-export interface Podcast {
-  id: string;
-  title: string,
-  autor: string,
-  imageSource: string,
-  description: string
-}
+const oneDayInMilliseconds = 86400000;
 
-export interface Episode {
-  title: string,
-  date: string,
-  duration: string,
-  id: string
-}
+export const getPodcastsFromService = async ({ filter }: { filter: string }) => {
+  const podcastFromLocalStorageString = localStorage.getItem('podcasts');
+  const podcastFromLocalStorage = podcastFromLocalStorageString ? JSON.parse(podcastFromLocalStorageString) : null;
 
-export interface PodcastDetail extends Podcast {
-  episodeCount: number,
-  episodes: Episode[]
+  if (podcastFromLocalStorage && (new Date() - new Date(podcastFromLocalStorage.date)) < oneDayInMilliseconds) {
+    console.log('Obteniendo detalle del podcast desde localStorage');
+    return podcastFromLocalStorage.data.filter((podcast: any) => podcast.title.toLowerCase().includes(filter.toLowerCase()) || podcast.autor.toLowerCase().includes(filter.toLowerCase()));
+  } else {
+
+    await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw new Error('Network response was not ok.')
+      })
+      .then(data => {
+        const podcasts = JSON.parse(data.contents).feed.entry
+        const mappedPodcasts = podcasts.map((podcast: any) => ({
+          id: podcast.id.attributes["im:id"],
+          title: podcast["im:name"].label,
+          autor: podcast["im:artist"].label,
+          imageSource: podcast["im:image"][2].label,
+          description: podcast.summary.label
+        }));
+        localStorage.setItem('podcasts', JSON.stringify({
+          data: mappedPodcasts,
+          date: new Date()
+        }));
+        return mappedPodcasts;
+      });
+  }
 }
